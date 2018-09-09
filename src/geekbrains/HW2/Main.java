0fba3cf1487c05715b8807788b5aaaf1dcf2f659
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-09-09 20:54:50 +0300

    created big array

---
+++ src/geekbrains/HW2/Main.java
@@ -0,0 +1,10 @@
+package geekbrains.HW2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] bigSize = new int [1000000]; //create big array
+        System.out.print("Array size: " + bigSize.length);
+        // write your code here
+    }
+}
