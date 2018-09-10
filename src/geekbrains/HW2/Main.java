dcd49e07290910afced23e5729e90a7d968cead9
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-09-10 10:50:39 +0300

    complete search and delete value

--- src/geekbrains/HW2/Main.java
+++ src/geekbrains/HW2/Main.java
@@ -1,12 +1,10 @@
 package geekbrains.HW2;
 
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        //create big array and link saved
-        int[] bigSize;
-        bigSize = new int [1000];
-        System.out.println("Array size: " + bigSize.length + " value");
     }
 }
